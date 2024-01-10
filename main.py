b15af9fc52555fe10da6f4590b41fce8add77018
mbsurfer <rneldner@gmail.com>
2024-01-10 14:53:36 -0500

    fixed new line characters

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from flashy_view import FlashyViewdef main():    view = FlashyView()    view.window.mainloop()if __name__ == "__main__":    main()
+from flashy_view import FlashyView
+
+
+def main():
+    view = FlashyView()
+    view.window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
